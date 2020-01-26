c6eda549065661850c1d369566ceaf5930a55c32
TymeeK <38594702+TymeeK@users.noreply.github.com>
2020-01-26 15:35:17 -0800

    Added musicframe class

---
+++ src/Frames/MusicFrame.java
@@ -0,0 +1,54 @@
+package Frames;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MusicFrame extends JFrame {
+    private JLabel titleLabel;
+    private JLabel searchLabel;
+    private JButton searchButton;
+    private JTextField searchField;
+    private static final int FRAME_WIDTH = 550;
+    private static final int FRAME_HEIGHT = 400;
+    private JPanel panel;
+
+    public MusicFrame() {
+
+        createComponents();
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setTitle("Home Page");
+    }
+
+    public void createComponents() {
+        MusicFrameListener listener = new MusicFrameListener();
+        panel = new JPanel();
+        titleLabel = new JLabel("Ghetto Spotify");
+        titleLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        panel.add(titleLabel);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        searchField = new JTextField(20);
+        panel.add(searchField);
+
+        searchButton = new JButton("Search");
+        searchButton.addActionListener(listener);
+        panel.add(searchButton);
+
+        this.add(panel);
+
+
+    }
+
+    class MusicFrameListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == searchButton) {
+                System.out.println("Search button was clicked");
+            }
+        }
+    }
+
+}
