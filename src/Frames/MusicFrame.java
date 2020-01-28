14f45dd1bf5e6e5c2ea68da00372974689b41219
Tanner-Mindrum <track2gamesofficial@gmail.com>
2020-01-28 14:25:06 -0800

    added horizontal box

--- src/Frames/MusicFrame.java
+++ src/Frames/MusicFrame.java
@@ -25,25 +25,36 @@ public class MusicFrame extends JFrame {
         MusicFrameListener listener = new MusicFrameListener();
         panel = new JPanel();
 
-        BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
-        panel.setLayout(boxLayout);
+        //BoxLayout boxLayout = new BoxLayout(panel, new FlowLayout());
+        panel.setLayout(new FlowLayout());
 
         titleLabel = new JLabel("Ghetto Spotify");
         titleLabel.setFont(new Font("Arial", Font.PLAIN, 18));
-        panel.add(titleLabel);
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //panel.add(titleLabel);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         searchField = new JTextField(20);
         searchField.setMaximumSize(searchField.getPreferredSize());
-        panel.add(searchField);
+        searchField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //panel.add(searchField);
 
         searchButton = new JButton("Search");
         searchButton.addActionListener(listener);
-        panel.add(searchButton);
+        //panel.add(searchButton);
 
-        Box playlistBox = Box.createHorizontalBox();
+        Box topBox = Box.createHorizontalBox();
+        topBox.setPreferredSize(new Dimension(1260, 80));
+        topBox.setBorder(BorderFactory.createLineBorder(Color.RED));
+        topBox.add(titleLabel);
+        topBox.add(searchField);
+        topBox.add(searchButton);
+        panel.add(topBox);
+
+        Box playlistBox = Box.createVerticalBox();
         playlistBox.add(new JTextField("Left"));
         playlistBox.setMaximumSize(playlistBox.getPreferredSize());
+        playlistBox.setAlignmentX(Component.LEFT_ALIGNMENT);
         panel.add(playlistBox, BorderLayout.CENTER);
 
         this.add(panel);
