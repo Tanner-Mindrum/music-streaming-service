7ab9aa61fd357417a5b24e6cf81b23ff1bc08dd2
TymeeK <38594702+TymeeK@users.noreply.github.com>
2020-02-25 13:46:08 -0800

    Changed Catalog and Proxy

--- src/Backend/Proxy.java
+++ src/Backend/Proxy.java
@@ -7,39 +7,27 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 public class Proxy {
 
-        private Catalog catalog;
+    private Catalog catalog;
 
-        public Proxy() throws IOException, ParseException {
-                catalog = new Catalog();
+    public Proxy() throws IOException, ParseException {
+        catalog = new Catalog();
+    }
+
+    public JSONObject synchExecution(String methodName, Object... param) throws InterruptedException {
+        HashMap<String, JSONObject> methods = catalog.getMethod();
+        
+
+        for (Object s : param) {
         }
 
-        public JSONObject synchExecution(String method, Object... param) throws InterruptedException {
-                JSONArray methods = catalog.getMethod();
-                for (Object m : methods) {
-                        JSONObject entryInfo = (JSONObject) m;
-                        Map rMethod = ((Map) entryInfo.get("remoteMethod"));
-                        Iterator<Map.Entry> rMethodItr = rMethod.entrySet().iterator();
-                        while (rMethodItr.hasNext()) {
-                                Map.Entry data = rMethodItr.next();
-                                if (data.getKey().equals("remoteMethod")) {
-                                        method = (String) data.getValue();
-                                        if (method.equals((String) data.getValue())) {
-
-                                        }
-                                }
-                        }
-                }
-
-                for (Object s : param) {
-                }
-
-                Thread thread = new Thread();
-                thread.join();
-        }
+        Thread thread = new Thread();
+        thread.join();
+    }
 
 }
